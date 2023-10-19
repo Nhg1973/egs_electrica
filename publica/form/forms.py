44b978a1f487e2c7b775252f14ad11a114a16a99
Nhg1973 <nahuelguerci@gmail.com>
2023-10-19 14:56:26 -0300

    con todos los productos

--- publica/form/forms.py
+++ publica/form/forms.py
@@ -4,42 +4,43 @@ class ContactForm(forms.Form):
     name = forms.CharField(
         label='Nombre',
         max_length=100,
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa tú nombre', 'required': True})
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingresá tu nombre', 'required': True})
     )
     email = forms.EmailField(
         label='Email',
         max_length=100,
-        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa una dirección de correo', 'required': True})
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Ingresá una dirección de correo', 'required': True})
     )
     subject = forms.CharField(
         label='Motivo',
         max_length=100,
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Motivo', 'required': True})
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Asunto', 'required': True})
     )
     message = forms.CharField(
         label='Mensaje',
-        widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'En que podemos ayudarte ', 'required': True})
+        widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'En qué podemos ayudarte ', 'required': True})
     )
 
 
 class PresupuestoForm(forms.Form):
     subject_choices = (
-    ('', 'Seleccione una opción'),
+    ('', 'Seleccioná una opción'),
     ('Insumos electricos', 'Insumos eléctricos'),
     ('servicio_instalacion', 'Servicio de instalación'),
     ('servicio_cableado', 'Servicio de cableado'),
     ('Asesoramiento', 'Asesoramiento'),
+    ('Otra consulta', 'Otra consulta'),
 )
 
     name = forms.CharField(
         label='Nombre',
         max_length=100,
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa tú nombre', 'required': True})
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingresá tu nombre', 'required': True})
     )
     email = forms.EmailField(
         label='Email',
         max_length=100,
-        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa tú dircceión de correo', 'required': True})
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Ingresá tu dirección de correo', 'required': True})
     )
 
 
